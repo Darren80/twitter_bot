b728e6df81ce4c3186546b479a04d71231ef3113
Darren Ohonba - Evans <we444465@gmail.com>
2023-07-22 18:35:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const app = require('express')();
+const cors = require('cors');
+const http = require('http').Server(app);
+require('dotenv').config()
+
+// import App from './TwitterBot';
+const App = require('./TwitterBot.js');
+
+const port = 1000;
+
+// Enable CORS from any origin
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+}
+);
+
+// Start the twitter bot
+app.get('/start', (req, res) => {
+    console.log('Starting the bot...');
+    App();
+    res.send('Starting the bot...');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
